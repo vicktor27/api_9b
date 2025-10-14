b5bc16556af23282f5c72421e4ef1c02601376e6
Vickthor3T <victor.cardoza@tripleten-team.com>
2025-10-14 13:51:17 -0600

    fix: db connect

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 import mongoose from "mongoose";
 
-const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/";
+const MONGO_URI = process.env.MONGO_URI;
 
 export const connectDB = async () => {
   try {
@@ -10,6 +10,6 @@ export const connectDB = async () => {
     await mongoose.connect(MONGO_URI);
     console.log("MongoDB Connected Succesfully!!!!");
   } catch (error) {
-    console.error("Mongo DB Connection Failed");
+    console.error("Mongo DB Connection Failed", error);
   }
 };
