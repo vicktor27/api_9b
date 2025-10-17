efd232b2079553e8dc31d0ea3df63e8ab14962ba
Vickthor3T <victor.cardoza@tripleten-team.com>
2025-10-17 07:58:45 -0600

    fix: db selected

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 import mongoose from "mongoose";
 
-const MONGO_URI = process.env.MONGO_URI;
+const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/";
 
 export const connectDB = async () => {
   try {
